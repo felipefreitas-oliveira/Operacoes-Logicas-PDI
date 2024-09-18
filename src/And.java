56746f779a7dc36d2859595e5106e898d0175c93
Felipe <feelipefst@gmail.com>
2024-09-18 11:54:31 -0300

    first commit

---
+++ src/And.java
@@ -0,0 +1,37 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class And {
+    public static void main(String[] args) throws IOException {
+        BufferedImage img1 = ImageIO.read(new File("src/img/1.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/2.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/3.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/4.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/5.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/6.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/7.jpg"));
+        BufferedImage img2 = ImageIO.read(new File("src/img/8.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/9.jpg"));
+        //BufferedImage img2 = ImageIO.read(new File("src/img/10.jpg"));
+
+        BufferedImage newImgAnd = new BufferedImage(img1.getWidth(), img1.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for (int x = 0; x < img1.getWidth(); x++) {
+            for (int y = 0; y < img1.getHeight(); y++) {
+
+                int pixelImg1 = img1.getRGB(x, y);
+                int pixelImg2 = img2.getRGB(x, y);
+
+                int resultAnd = pixelImg1 & pixelImg2;
+
+                newImgAnd.setRGB(x, y, resultAnd);
+
+            }
+        }
+
+        ImageIO.write(newImgAnd, "jpg", new File("src/imgAnd.jpg"));
+
+    }
+}
---
+++ src/Sub.java
@@ -0,0 +1,67 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Sub {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedImage img1 = ImageIO.read(new File("src/img/4.jpg"));
+        BufferedImage img2 = ImageIO.read(new File("src/img/8.jpg"));
+
+        ArrayList<Integer> matrizCores = new ArrayList<>();
+
+        BufferedImage newImgSub = new BufferedImage(img1.getWidth(), img1.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        for(int x = 0; x < img1.getWidth(); x++){
+            for(int y = 0; y < img1.getHeight(); y++){
+
+                int pixel = img1.getRGB(x, y);
+                int pixel2 = img2.getRGB(x, y);
+
+                Color cor = new Color(pixel);
+                Color cor2 = new Color(pixel2);
+
+                int r = cor.getRed() - cor2.getRed();
+
+                matrizCores.add(r);
+            }
+        }
+
+        int fmax = 0;
+        int fmin = 255;
+
+        for (int c = 0; c < matrizCores.size(); c++){
+            if (matrizCores.get(c) > fmax){
+                fmax = matrizCores.get(c);
+            }
+            if (matrizCores.get(c) < fmin){
+                fmin = matrizCores.get(c);
+            }
+        }
+
+        System.out.println("aqui " + fmax);
+        System.out.println("aqui " + fmin);
+
+        int aux = 0;
+
+        for(int x = 0; x < img1.getWidth(); x++){
+            for(int y = 0; y < img1.getHeight(); y++){
+
+                int f = matrizCores.get(aux);
+
+                double rs = 255.0 / (fmax - fmin);
+                int g = (int) (rs * (f - fmin));
+
+                aux++;
+
+                newImgSub.setRGB(x, y, new Color(g, g, g).getRGB());
+            }
+        }
+
+        ImageIO.write(newImgSub, "jpg", new File("src/imgSUB.jpg"));
+    }
+}
